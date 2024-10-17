5cc079412e132fad764c1c521af1ab1d7cf4a5cf
Gautam27021909 <kachhadiyagd@gmail.com>
2024-10-17 14:28:46 -0500

    commit

--- RatingSystem/RateRatePro/static/js/LoginPage.js
+++ RatingSystem/RateRatePro/static/js/LoginPage.js
@@ -3,13 +3,12 @@ import { useNavigate } from 'react-router-dom';
 import './LoginPage.css';
 
 const LoginPage = () => {
-    const navigate = useNavigate();  // To handle navigation after login
+    const navigate = useNavigate(); 
 
     const handleLogin = (e) => {
         e.preventDefault();
-        // Logic for handling login, like authentication
-        // After successful login:
-        navigate('/landing');  // Navigate to the landing page after login
+       
+        navigate('/landing');
     };
 
 };
